22916412a926785b19ad745a60aa39b6edd25813
Austromo <moritz.gruber.linz@gmail.com>
2024-11-30 18:46:52 +0100

    fix: make the pages scrollable
    - change color scheme to deep orange
    
    - change font sizes of header and itn change

--- itn_calculator_flutter_app/lib/main.dart
+++ itn_calculator_flutter_app/lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         title: 'ITN Calculator',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
       ),
@@ -145,9 +145,13 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [page],
+              child: Center(
+                child: ListView(shrinkWrap: true, children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [page],
+                  ),
+                ]),
               ),
             )),
       );
@@ -169,7 +173,7 @@ class WelcomeHomePage extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.all(20),
                     child: Text(
-                      "Welcome to ITN Calculator",
+                      'Welcome to ITN Calculator',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 30.0,
@@ -192,140 +196,131 @@ class _ITNCalculatorPageState extends State<ITNCalculatorPage> {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    return Align(
-      alignment: Alignment.center,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-          child: ColoredBox(
-              color: Theme.of(context).cardColor.withOpacity(0.5),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(50, 20, 50, 20),
+      child: ColoredBox(
+          color: Theme.of(context).cardColor.withOpacity(0.5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              Text(
+                'ITN Calculator',
+                style: TextStyle(color: Colors.black, fontSize: 35.0),
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(height: 30),
-                  Text(
-                    "ITN Calculator",
-                    style: TextStyle(color: Colors.black, fontSize: 30.0),
-                  ),
-                  SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ToggleButtons(
-                        direction: Axis.horizontal,
-                        onPressed: (int index) {
-                          setState(() {
-                            // The button that is tapped is set to true, and the others to false.
-                            for (int i = 0;
-                                i < appState.selectedMode.length;
-                                i++) {
-                              appState.selectedMode[i] = i == index;
-                            }
+                  ToggleButtons(
+                    direction: Axis.horizontal,
+                    onPressed: (int index) {
+                      setState(() {
+                        // The button that is tapped is set to true, and the others to false.
+                        for (int i = 0; i < appState.selectedMode.length; i++) {
+                          appState.selectedMode[i] = i == index;
+                        }
 
-                            appState.calculateItnChange(
-                                appState.selectedSuccess[0]);
-                          });
-                        },
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8)),
-                        selectedBorderColor:
-                            Theme.of(context).colorScheme.onPrimaryContainer,
-                        selectedColor: Colors.black,
-                        fillColor: Theme.of(context).colorScheme.inversePrimary,
-                        color: Theme.of(context).colorScheme.secondary,
-                        borderWidth: 2,
-                        isSelected: appState.selectedMode,
-                        children: modeIcons,
-                      ),
-                      SizedBox(width: 60),
-                      ToggleButtons(
-                        direction: Axis.horizontal,
-                        onPressed: (int index) {
-                          setState(() {
-                            // The button that is tapped is set to true, and the others to false.
-                            for (int i = 0;
-                                i < appState.selectedSuccess.length;
-                                i++) {
-                              appState.selectedSuccess[i] = i == index;
-                            }
-
-                            appState.calculateItnChange(
-                                appState.selectedSuccess[0]);
-                          });
-                        },
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8)),
-                        selectedBorderColor:
-                            Theme.of(context).colorScheme.onPrimaryContainer,
-                        fillColor: Theme.of(context).colorScheme.inversePrimary,
-                        borderWidth: 2,
-                        isSelected: appState.selectedSuccess,
-                        children: successIcons,
-                      ),
-                    ],
+                        appState
+                            .calculateItnChange(appState.selectedSuccess[0]);
+                      });
+                    },
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    selectedBorderColor:
+                        Theme.of(context).colorScheme.onPrimaryContainer,
+                    selectedColor: Colors.black,
+                    fillColor: Theme.of(context).colorScheme.inversePrimary,
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderWidth: 2,
+                    isSelected: appState.selectedMode,
+                    children: modeIcons,
                   ),
-                  SizedBox(height: 20),
-                  Padding(
+                  SizedBox(width: 60),
+                  ToggleButtons(
+                    direction: Axis.horizontal,
+                    onPressed: (int index) {
+                      setState(() {
+                        // The button that is tapped is set to true, and the others to false.
+                        for (int i = 0;
+                            i < appState.selectedSuccess.length;
+                            i++) {
+                          appState.selectedSuccess[i] = i == index;
+                        }
+
+                        appState
+                            .calculateItnChange(appState.selectedSuccess[0]);
+                      });
+                    },
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    selectedBorderColor:
+                        Theme.of(context).colorScheme.onPrimaryContainer,
+                    fillColor: Theme.of(context).colorScheme.inversePrimary,
+                    borderWidth: 2,
+                    isSelected: appState.selectedSuccess,
+                    children: successIcons,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                child: getITNTextFormField(
+                    appState,
+                    'Your ITN',
+                    appState.itnUser == 0
+                        ? ''
+                        : appState.itnUser.toStringAsFixed(3),
+                    (value) => appState.itnUser = value),
+              ),
+              if (appState.selectedMode[1]) ...[
+                SizedBox(height: 20),
+                Padding(
                     padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
                     child: getITNTextFormField(
                         appState,
-                        'Your ITN',
-                        appState.itnUser == 0
+                        'ITN of your partner',
+                        appState.itnPartner == 0
                             ? ''
-                            : appState.itnUser.toStringAsFixed(3),
-                        (value) => appState.itnUser = value),
-                  ),
-                  if (appState.selectedMode[1]) ...[
-                    SizedBox(height: 20),
-                    Padding(
-                        padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                        child: getITNTextFormField(
-                            appState,
-                            'ITN of your partner',
-                            appState.itnPartner == 0
-                                ? ''
-                                : appState.itnPartner.toStringAsFixed(3),
-                            (value) => appState.itnPartner = value))
-                  ],
-                  SizedBox(height: 20),
-                  Padding(
-                      padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                      child: getITNTextFormField(
-                          appState,
-                          'ITN of your opponent',
-                          appState.itnOpponent1 == 0
-                              ? ''
-                              : appState.itnOpponent1.toStringAsFixed(3),
-                          (value) => appState.itnOpponent1 = value)),
-                  if (appState.selectedMode[1]) ...[
-                    SizedBox(height: 20),
-                    Padding(
-                        padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                        child: getITNTextFormField(
-                            appState,
-                            'ITN of your opponent',
-                            appState.itnOpponent2 == 0
-                                ? ''
-                                : appState.itnOpponent2.toStringAsFixed(3),
-                            (value) => appState.itnOpponent2 = value))
-                  ],
-                  SizedBox(height: 20),
-                  Text(
-                      style: TextStyle(
-                          fontSize: 40,
-                          color: appState.selectedSuccess[0]
-                              ? Colors.green
-                              : Colors.red),
-                      appState.itnChangeOfUser.isNaN
+                            : appState.itnPartner.toStringAsFixed(3),
+                        (value) => appState.itnPartner = value))
+              ],
+              SizedBox(height: 20),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                  child: getITNTextFormField(
+                      appState,
+                      'ITN of your opponent',
+                      appState.itnOpponent1 == 0
                           ? ''
-                          : appState.selectedSuccess[0]
-                              ? '- ${appState.itnChangeOfUser.toStringAsFixed(3)} ⟶ ${(appState.itnUser - appState.itnChangeOfUser).toStringAsFixed(3)}'
-                              : '+ ${appState.itnChangeOfUser.toStringAsFixed(3)} ⟶ ${(appState.itnUser + appState.itnChangeOfUser).toStringAsFixed(3)}'),
-                  SizedBox(height: 20),
-                ],
-              )),
-        ),
-      ),
+                          : appState.itnOpponent1.toStringAsFixed(3),
+                      (value) => appState.itnOpponent1 = value)),
+              if (appState.selectedMode[1]) ...[
+                SizedBox(height: 20),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                    child: getITNTextFormField(
+                        appState,
+                        'ITN of your opponent',
+                        appState.itnOpponent2 == 0
+                            ? ''
+                            : appState.itnOpponent2.toStringAsFixed(3),
+                        (value) => appState.itnOpponent2 = value))
+              ],
+              SizedBox(height: 20),
+              Text(
+                  style: TextStyle(
+                      fontSize: 35,
+                      color: appState.selectedSuccess[0]
+                          ? Colors.green
+                          : Colors.red),
+                  appState.itnChangeOfUser.isNaN
+                      ? ''
+                      : appState.selectedSuccess[0]
+                          ? '- ${appState.itnChangeOfUser.toStringAsFixed(3)} ⟶ ${(appState.itnUser - appState.itnChangeOfUser).toStringAsFixed(3)}'
+                          : '+ ${appState.itnChangeOfUser.toStringAsFixed(3)} ⟶ ${(appState.itnUser + appState.itnChangeOfUser).toStringAsFixed(3)}'),
+              SizedBox(height: 20),
+            ],
+          )),
     );
   }
 
@@ -344,7 +339,7 @@ class _ITNCalculatorPageState extends State<ITNCalculatorPage> {
       ],
       autovalidateMode: AutovalidateMode.always,
       validator: (value) {
-        double itn = double.tryParse(value ?? "") ?? 0;
+        double itn = double.tryParse(value ?? '') ?? 0;
         return itn >= minItn && itn <= maxItn || value == ''
             ? null
             : 'Invalid value';
